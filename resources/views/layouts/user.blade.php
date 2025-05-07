ed5c1e36fff8c242e2e0f2779fa7cb37ec701d97
subash255 <suad_bca2077@lict.edu.np>
2025-05-07 14:50:47 +0545

    new

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -55,7 +55,8 @@
                         </li>
                         
                         <li>
-                            <form action="{{route('logout')}}" method="post">
+                            
+                             <form action="{{route('logout')}}" method="post">
                                 @csrf
                                 <button class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Logout</button>
 
